2aa13df17322b91cd9d8445efcf87b1f58397fcd
vcmsss <pak.al.ig@yandex.ru>
2019-07-17 15:08:34 +0300

    задача 4

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        String bin = in.nextLine();
+
+        int binNum = Integer.parseInt(bin, 2);
+
+
+
+        System.out.println(binNum);
+
+    }
+
+
+
+}
